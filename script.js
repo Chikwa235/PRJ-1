f9d698168bd677ee6104029098abc3076ce1580c
Chikwa235 <chisapachikwa25@gmail.com>
2025-07-19 20:47:22 +0200

    Implement dynamic addition of education input blocks

--- script.js
+++ script.js
@@ -10,4 +10,35 @@ function isValidPhone(phone) {
   const phoneRegex = /^\+?[0-9]{7,15}$/;
   return phoneRegex.test(phone);
 }
+
+ function addEducation() {
+  let eduCount = parseInt(document.getElementById('eduCount').value);
+  eduCount++;
+  document.getElementById('eduCount').value = eduCount;
+
+  const container = document.getElementById('educationContainer');
+  const newBlock = document.createElement('div');
+  newBlock.className = 'input-group';
+  newBlock.id = `eduBlock${eduCount}`;
+  newBlock.innerHTML = `
+    <label>Institution</label>
+    <input type="text" id="institution${eduCount}" />
+
+    <label>Degree / Diploma (optional)</label>
+    <input type="text" id="degree${eduCount}" />
+
+    <label>Field of Study / Major</label>
+    <input type="text" id="field${eduCount}" />
+
+    <label>Year Started</label>
+    <input type="text" id="yearStart${eduCount}" />
+
+    <label>Year Completed</label>
+    <input type="text" id="yearEnd${eduCount}" />
+
+    <label>Grade / GPA (optional)</label>
+    <input type="text" id="grade${eduCount}" />
+  `;
+  container.appendChild(newBlock);
+}
 });
